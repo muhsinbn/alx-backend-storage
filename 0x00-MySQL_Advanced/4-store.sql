d8e66accc93d1b8e4a8cbc694242ef1466a9adaa
Mohammmed Yahaya <muhsinbnyahya@gmail.com>
2024-08-23 23:14:26 +0100

    4-store.sql

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,15 +1,11 @@
--- Initial
-DROP TABLE IF EXISTS items;
-DROP TABLE IF EXISTS orders;
+-- Write a SQL script that creates a trigger
+-- trigger decreases the quantity of an item after adding a new order.
+-- Quantity in the table items can be negative.
 
-CREATE TABLE IF NOT EXISTS items (
-    name VARCHAR(255) NOT NULL,
-    quantity int NOT NULL DEFAULT 10
-);
-
-CREATE TABLE IF NOT EXISTS orders (
-	    item_name VARCHAR(255) NOT NULL,
-	    number int NOT NULL
-);
-
-INSERT INTO items (name) VALUES ("apple"), ("pineapple"), ("pear");
+-- Create trigger to decrease item quantity after adding a new order
+CREATE TRIGGER decrease_item_quantity AFTER INSERT ON orders
+FOR EACH ROW
+-- Update the quantity of the item in the items table
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
